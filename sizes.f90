a41a01bee77acd63715d0c821c79b1d299866bdd
garrelt <garrelt>
2006-10-21 17:15:47 +0000

    Mesh should be public

--- sizes.f90
+++ sizes.f90
@@ -15,7 +15,7 @@ module sizes
 
   !Size of the mesh for spatial coordinate.
   integer,dimension(Ndim),parameter,public :: mesh=(/ 203, 203, 203 /)
-  !integer,dimension(Ndim),parameter :: mesh=(/406, 406, 406/)
-  !integer,dimension(Ndim),parameter :: mesh=(/812, 812, 812/)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/406, 406, 406/)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/812, 812, 812/)
 
 end module sizes
