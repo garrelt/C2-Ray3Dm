93edf0a3822bffeb99a6027eeaa8c24b976ff927
garrelt <garrelt>
2007-06-15 18:57:27 +0000

    The no MPI version also needs to be preprocessed because of OpenMP calls. This is the continuation of no_mpi.f90

---
+++ no_mpi.F90
@@ -0,0 +1,144 @@
+module my_mpi
+
+  ! Module for Capreole (3D)
+  ! Author: Garrelt Mellema
+  ! Date: 2003-06-01
+  ! This module is also accepted by the F compiler (Dec 9, 2003)
+ 
+  ! This is a dummy module for systems where there is no MPI
+  ! for F90.
+  !
+  !----------------------------------------------------------------------------
+  
+
+  !private
+
+#ifdef XLF
+  USE XLFUTILITY, only: hostnm => hostnm_ , flush => flush_
+#else
+  USE OMP_LIB, only: omp_get_num_threads, omp_get_thread_num
+  USE IFPORT, only: hostnm, flush
+#endif
+
+  implicit none
+
+  integer,parameter,public :: NPDIM=3 ! dimension of problem
+
+  integer,public :: rank              ! rank of the processor
+  integer,public :: npr               ! number of processors
+  integer,public :: nthreads        ! number of threads (per processor)
+  integer,public :: MPI_COMM_NEW      ! the (new) communicator
+  integer,dimension(NPDIM),public :: dims ! number of processors in 
+                                             !  each dimension
+  integer,dimension(NPDIM),public :: grid_struct ! coordinates of 
+                                               !the processors in the grid
+  
+  integer,public ::  nbrleft,nbrright ! left and right neighbours
+  integer,public ::  nbrdown,nbrup    ! up and down neighbours 
+  integer,public ::  nbrabove,nbrbelow        ! above and below neighbours 
+
+  integer,parameter,public :: MPI_PROC_NULL=-1
+
+contains
+
+  !----------------------------------------------------------------------------
+
+  subroutine mpi_setup ( )
+
+    character(len=10) :: filename        ! name of the log file
+    character(len=4) :: number
+    integer :: ierror
+    integer :: tn
+    character(len=100) :: hostname
+
+    call mpi_basic ()
+
+    ! Open processor dependent log file
+    write(unit=number,fmt="(I4)") rank
+    filename=trim(adjustl("log."//trim(adjustl(number))))
+    open(unit=30,file=filename,status="unknown",action="write")
+
+    !$omp parallel default(shared)
+    nthreads=omp_get_num_threads()
+    !$omp end parallel
+    write(30,*) ' Number of OpenMP threads is ',nthreads
+    write(unit=30,fmt=*) "Log file for rank ",rank
+
+    ! Figure out hostname
+    ! NOTE: compiler dependent!!!
+    !$omp parallel default(shared)
+    tn=omp_get_thread_num()+1
+    ierror=hostnm(hostname)
+    if (ierror == 0) then
+       write(30,*) 'Thread number ',tn,' running on Processor ',hostname
+    else 
+       write(30,*) 'Error establishing identity of processor for thread ',tn
+    endif
+    !$omp end parallel
+
+    call flush(30)
+
+    call mpi_topology ()
+
+  end subroutine mpi_setup
+
+  !----------------------------------------------------------------------------
+
+  subroutine mpi_basic ( )
+
+    rank=0 ! Find processor rank
+
+    ! Find total number of processors (npr)
+    npr=1
+
+  end subroutine mpi_basic
+
+  !----------------------------------------------------------------------------
+
+  subroutine mpi_topology ( )
+
+
+    ! Make a new topology
+    dims=1
+
+    ! makes MPI_COMM_NEW    
+    MPI_COMM_NEW=0
+
+    ! makes grid_struct               
+    grid_struct=0
+      
+    ! Find the neighbours.
+    ! My neighbors are now +/- 1 with my rank. Handle the case of the 
+    ! boundaries by using MPI_PROC_NULL.
+    call fnd2dnbrs ( )
+
+  end subroutine mpi_topology
+
+  !----------------------------------------------------------------------------
+
+  subroutine mpi_end ( )
+
+    ! Close log file
+    close(30)
+
+  end subroutine mpi_end
+
+  !----------------------------------------------------------------------------
+
+  subroutine fnd2dnbrs ( )
+    
+    ! This routine determines the neighbours in a 2-d decomposition of
+    ! the grid. This assumes that MPI_Cart_create has already been called 
+
+    ! Single processor version
+
+    nbrleft=MPI_PROC_NULL
+    nbrright=MPI_PROC_NULL
+    nbrup=MPI_PROC_NULL
+    nbrdown=MPI_PROC_NULL
+    nbrabove=MPI_PROC_NULL
+    nbrbelow=MPI_PROC_NULL
+    
+  end subroutine fnd2dnbrs
+
+end module my_mpi
