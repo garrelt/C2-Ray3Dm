a55e32ae6a9b72a721391ca96d1f08df11ed80aa
garrelt <garrelt>
2007-06-15 19:02:08 +0000

    cosmoparms files for three different cases

---
+++ cosmoparms_WMAP1.f90
@@ -0,0 +1,25 @@
+module cosmology_parameters
+
+  ! This module defines essential cosmological parameters
+
+  ! Version: WMAP3
+
+  use precision, only: dp
+  use mathconstants, only: pi
+  use cgsconstants, only: G_grav
+  use astroconstants, only: Mpc
+
+  implicit none
+
+  ! WMAP1
+
+  real(kind=dp),parameter :: h=0.7 ! Hubble constant (in 100 km/s/Mpc)
+  real(kind=dp),parameter :: Omega0=0.27 ! Total matter density (in critical density)
+  real(kind=dp),parameter :: Omega_B=0.02156/(h*h) ! Baryon density (in critical density)
+  real(kind=dp),parameter :: cmbtemp=2.726 ! CMB temperature
+
+  ! Derived parameters
+  real(kind=dp),parameter :: H0=h*100.0*1e5/Mpc ! Hubble constant (cgs)
+  real(kind=dp),parameter :: rho_crit_0=3.0*H0*H0/(8.0*pi*G_grav) ! critical density (cgs)
+
+end module cosmology_parameters
