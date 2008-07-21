dc035320de2cb8da0addb7e3c40fbc108bc9f78d
garrelt <garrelt>
2008-07-21 15:18:08 +0000

    Files needed by doxygen

---
+++ mainpage.h
@@ -0,0 +1,38 @@
+/** @mainpage C2-Ray (3Dm)
+*
+* @authors Garrelt Mellema & Ilian T. Iliev
+*
+* @section intro Introduction
+* This program performs 3-dimensional time-dependent photo-ionization
+* calculations for cosmological reionization.
+* It needs a density fields and source lists, calculated from an N-body
+* simulation. Different types of N-body output are available, and have
+* be specified at compile time: pmfast, cubep3m, LG
+*
+* Examples of its use can be found in:
+*
+* <em>Simulating cosmic reionization at large scales I: the geometry of reionization</em>\n
+I. T. Iliev, G. Mellema, U.-L. Pen, H. Merz, P. R. Shapiro, M. A. Alvarez, 2006\n
+MNRAS 369, 1625
+*
+* <em>Simulating Cosmic Reionization at Large Scales II: the 21-cm Emission Features and Statistical Signals</em>\n
+G. Mellema, I. T. Iliev, U.-L. Pen, H. Merz, P. R. Shapiro, 2006\n
+MNRAS 372, 679</em>\n
+*
+* No hydrodynamics is included.
+*
+* Sample input files can be found in the \c inputs directory
+*
+* Compile information can be found in the Makefile
+*
+* <hr>
+* @section notes release.notes
+* release.notes
+* <hr>
+* @section requirements requirements
+* @verbinclude requirements
+* <hr> 
+* @todo [optionally include text about more work to be done]
+* @todo Give each todo item its own line
+*
+*/
