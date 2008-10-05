3010e7303b5b71ce43775ef6678f73c78882a850
garrelt <garrelt>
2008-10-05 14:28:40 +0000

    Making WMAP3+ the standard cosmoparms.

--- cosmoparms.f90
+++ cosmoparms.f90
@@ -1,17 +1,8 @@
-!>
-!! \brief This module defines essential cosmological parameters
-!!
-!! Module for Capreole / C2-Ray (f90)
-!!
-!! \b Author: Garrelt Mellema
-!!
-!! \b Date: 
-!!
 module cosmology_parameters
 
   ! This module defines essential cosmological parameters
 
-  ! Version: WMAP3
+  ! Version: WMAP3+
 
   use precision, only: dp
   use mathconstants, only: pi
@@ -20,15 +11,17 @@ module cosmology_parameters
 
   implicit none
 
-  ! WMAP3
+  ! WMAP3+; Note: Omega_lambda=1-Omega_0 and for this model sigma8=0.8, n_s=0.96.
 
-  real(kind=dp),parameter :: h=0.73 !< Hubble constant (in 100 km/s/Mpc)
-  real(kind=dp),parameter :: Omega0=0.24 !< Total matter density (in critical density)
-  real(kind=dp),parameter :: Omega_B=0.0223/(h*h) !< Baryon density (in critical density)
-  real(kind=dp),parameter :: cmbtemp=2.726 !< CMB temperature
+  real(kind=dp),parameter :: h=0.7 ! Hubble constant (in 100 km/s/Mpc)
+  !real(kind=dp),parameter :: Omega0=0.238 ! Total matter density (in critical density)
+  real(kind=dp),parameter :: Omega0=0.27 ! Total matter density (in critical density)
+  !real(kind=dp),parameter :: Omega_B=0.0418 ! Baryon density (in critical density)
+  real(kind=dp),parameter :: Omega_B=0.044 ! Baryon density (in critical density)
+  real(kind=dp),parameter :: cmbtemp=2.726 ! CMB temperature
 
   ! Derived parameters
-  real(kind=dp),parameter :: H0=h*100.0*1e5/Mpc !< Hubble constant (cgs)
-  real(kind=dp),parameter :: rho_crit_0=3.0*H0*H0/(8.0*pi*G_grav) !< critical density (cgs)
+  real(kind=dp),parameter :: H0=h*100.0*1e5/Mpc ! Hubble constant (cgs)
+  real(kind=dp),parameter :: rho_crit_0=3.0*H0*H0/(8.0*pi*G_grav) ! critical density (cgs)
 
 end module cosmology_parameters
