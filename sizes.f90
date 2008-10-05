d0a1178c48e438ac379432ae49c8392682244e25
garrelt <garrelt>
2008-10-05 14:21:00 +0000

    Bigger collection of mesh sizes for different simulations, annotated.

--- sizes.f90
+++ sizes.f90
@@ -8,14 +8,30 @@ module sizes
   ! This module contains basic parameter definitions
   
   ! use precision
+
   implicit none
 
   private
   integer,parameter,public :: Ndim=3
 
   !Size of the mesh for spatial coordinate.
-  integer,dimension(Ndim),parameter,public :: mesh=(/ 203, 203, 203 /)
+  !for 3072^3 particles, 6144^3 grid, 114/h Mpc box
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 256, 256, 256 /)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 384, 384, 384 /)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 512, 512, 512 /)
+
+  !for 1728^3 particles, 3456^3 grid, 64/h Mpc box
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 216, 216, 216 /)
+  integer,dimension(Ndim),parameter,public :: mesh=(/ 432, 432, 432 /)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 864, 864, 864 /)
+
+  !for LG simulation: 1024^3 particles, 1024^3 grid, 64/h Mpc box
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 512, 512, 512 /)
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 256, 256, 256 /)
   !integer,dimension(Ndim),parameter,public :: mesh=(/ 128, 128, 128 /)
+
+  !for old-style PMFAST simulation, 1624^3 particles, 3248^3 grid, 100/h or 35/h Mpc box
+  !integer,dimension(Ndim),parameter,public :: mesh=(/ 203, 203, 203 /)
   !integer,dimension(Ndim),parameter,public :: mesh=(/406, 406, 406/)
   !integer,dimension(Ndim),parameter,public :: mesh=(/812, 812, 812/)
 
