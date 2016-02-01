52d32bb4c78b109c81720c7bef3c81aa84b543fe
Garrelt Mellema <garrelt@astro.su.se>
2016-02-01 17:51:43 +0100

    Modifications due to synchronizing with the H+He version. This include the addition of the new module for reading sm3d files. This module has not yet been fully incorporated into the code base. For example it does not yet get mentioned in the Makefile! So the code will not compile

---
+++ read_sm3d.f90
@@ -0,0 +1,62 @@
+module read_sm3d
+
+use precision, only: dp,si
+
+contains
+  subroutine read_sm3d_dp_file_routine(filename,data_array)
+
+    character(len=*),intent(in) :: filename
+    real(kind=dp),pointer,dimension(:,:,:),intent(inout) :: data_array
+
+    integer :: m1,m2,m3
+    integer,dimension(3) :: shape_of_data
+
+    shape_of_data=shape(data_array)
+
+    ! Open sm3d data file
+    open(unit=20,file=filename,form="unformatted",status="old")       
+    
+    ! Read in data
+    read(20) m1,m2,m3
+    if (m1 /= shape_of_data(1) .or. m2 /= shape_of_data(2) .or. &
+         m3 /= shape_of_data(3)) then
+       write(logf,*) "Warning: file ",filename," unusable."
+       write(logf,*) "since mesh found in file: ",m1,m2,m3
+       write(logf,*) "and ",shape_of_data(:)," needed."
+    else
+       read(20) data_array
+    endif
+    ! close file
+    close(20)
+
+  end subroutine read_sm3d_dp_file_routine
+
+  subroutine read_sm3d_si_file_routine(filename,data_array)
+
+    character(len=*),intent(in) :: filename
+    real(kind=si),pointer,dimension(:,:,:),intent(inout) :: data_array
+
+    integer :: m1,m2,m3
+    integer,dimension(3) :: shape_of_data
+
+    shape_of_data=shape(data_array)
+
+    ! Open sm3d data file
+    open(unit=20,file=filename,form="unformatted",status="old")       
+    
+    ! Read in data
+    read(20) m1,m2,m3
+    if (m1 /= shape_of_data(1) .or. m2 /= shape_of_data(2) .or. &
+         m3 /= shape_of_data(3)) then
+       write(logf,*) "Warning: file ",filename," unusable."
+       write(logf,*) "since mesh found in file: ",m1,m2,m3
+       write(logf,*) "and ",shape_of_data(:)," needed."
+    else
+       read(20) data_array
+    endif
+    ! close file
+    close(20)
+
+  end subroutine read_sm3d_si_file_routine
+
+end module read_sm3d
